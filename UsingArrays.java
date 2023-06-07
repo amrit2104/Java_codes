b58bf896e0138610081ec054ea8642d40ecae885
Amrit Gaurav Rath <NH1965@nslhub.in>
2023-06-07 15:29:26 +0530

    completed array input and output

--- UsingArrays.java
+++ UsingArrays.java
@@ -1,5 +1,20 @@
-import java.io.*;
+import java.util.*;
+
 public class UsingArrays 
 {
-    
+    public static void main(String args[])
+    {
+        try (Scanner x = new Scanner(System.in)) {
+            System.out.println("Enter a number :");
+            int n=x.nextInt();
+
+            // input in array
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++)
+            arr[i] = x.nextInt();
+
+            for(int i=0;i<n;i++)
+            System.out.println(arr[i]);
+        }
+    }    
 }
